6c585b3f570b77478499972b458c6c965228c7ee
Syu <58730464+texture08@users.noreply.github.com>
2021-07-11 09:47:26 +0900

    Add files via upload

---
+++ scripts/footer.js
@@ -0,0 +1,4 @@
+document.write(`<footer>
+        <p>シューのリソース工房<span class="mgr-10">お問い合わせ</span></p>
+        <p><a href="https://twitter.com/H2DH8K" target="_blank" class="color1">Twitter</a> ID:H2DH8K<br> <a href="https://discord.gg/FuDaAWaNAV" target="_blank" class="color1">Discord</a> ID:シュー#8493<br> <span class="mini">&copy; 2021〜</span></p>
+    </footer>`)
